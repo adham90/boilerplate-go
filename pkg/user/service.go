b0a410755ec37154bb7de4c9079ec52494cef170
adham90 <adham.eldeeb90@gmail.com>
2018-06-09 12:02:35 +0200

    add readme file

--- pkg/user/service.go
+++ pkg/user/service.go
@@ -5,7 +5,7 @@ import e "github.com/adham90/boilerplate/pkg/entity"
 //Service service interface
 type Service interface {
 	Find(id string) (*e.User, error)
-	// Register(user *e.User) (e.User, error)
+	Register(user *e.User) (*e.User, error)
 	// Validate(user *e.User) error
 	// Auth(user *e.User) error
 	// GetRepo() Repository
